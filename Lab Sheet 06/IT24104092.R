9923639244fac4399adfeb05f1f73fc2a62b2088
IT24104092 <it24104092@my.sliit.lk>
2025-09-15 23:46:56 +0530

    IT24104092_Lab 6

---
+++ Lab Sheet 06/IT24104092.R
@@ -0,0 +1,21 @@
+setwd("C:\\Users\\acer\\Desktop\\IT24104092")
+#Exercise 
+#Q1
+
+#1)
+0.85
+
+#2)
+dbinom(47,50,0.85)
+
+#Q2
+
+#1)
+
+# X=number of customer calls received in a given time period
+
+#2)
+12
+
+#3)
+dpois(15,12)
